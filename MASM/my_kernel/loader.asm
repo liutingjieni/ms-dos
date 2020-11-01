aa21d2737a44be057380f3602701211f71ce6bd4
liutingjieni <644123183@qq.com>
2020-11-01 23:08:59 +0800

    获取物理内存容量

--- MASM/my_kernel/loader.asm
+++ MASM/my_kernel/loader.asm
@@ -28,12 +28,90 @@ SELECTOR_CODE equ (0x0001 << 3) + TI_GDT + RPL0
 SELECTOR_DATA equ (0x0002 << 3) + TI_GDT + RPL0
 SELECTOR_VIDEO equ (0x0003 << 3) + TI_GDT + RPL0
 
+total_mem_bytes dd 0 ;当前内存地址0xb00
+
 gdt_ptr dw GDT_LIMIT 
         dd GDT_BASE
+
+;人工对齐
+ards_buf times 244 db 0
+ards_nr dw 0
 
 loadermsg db '2 loader in real'
 
 loader_start:
+    ;三种方法获取内存大小, biosz中断0x15子功能0xe820 0xe801 0x88 
+    xor ebx, ebx
+    mov edx, 0x534d4150 ;固定为签名标记
+    mov di, ards_buf    ;ards缓冲区
+.e820_mem_get_loop:
+    mov eax, 0x0000e820
+    mov ecx, 20
+    int 0x15
+    jc .e820_failed_so_try_e801
+    add di, cx
+    inc word [ards_nr]
+    cmp ebx, 0
+    jnz .e820_mem_get_loop
+
+    mov cx, [ards_nr]
+    mov ebx, ards_buf
+    xor edx, edx
+
+.find_max_mem_area:
+    mov eax, [ebx]
+    add eax, [ebx + 8]
+    add ebx, 20
+    cmp edx, eax
+
+    jge .next_ards
+    mov edx, eax
+
+.next_ards:
+    loop .find_max_mem_area
+    jmp .mem_get_ok
+
+.e820_failed_so_try_e801:
+    mov ax, 0xe801
+    int 0x15
+    jc .e801_failed_so_try88
+
+    mov cx, 0x400
+    mul cx
+    shl edx, 16
+    and eax, 0x0000ffff
+    or edx, eax
+    add edx, 0x100000
+    mov esi, edx
+
+    xor eax, eax
+    mov ax, bx
+    mov ecx, 0x10000 
+    mul ecx
+
+    add esi, eax
+    
+    mov edx, esi
+    jmp .mem_get_ok
+
+.e801_failed_so_try88:
+    mov ah, 0x88
+    int 0x15
+    jc .error_hlt
+    and eax, 0x0000ffff
+
+    mov cx, 0x400
+    mul cx
+    shl edx, 16
+    or edx, eax
+    add edx, 0x100000
+
+.mem_get_ok:
+    mov [total_mem_bytes], edx
+
+.error_hlt:
+    ;hlt
+
     ;------------------------------------------
     ;int 0x10  功能号:0x13  功能描述:打印字符串
     ;-----------------------------------------
@@ -74,5 +152,6 @@ p_mode_start:
     mov gs, ax
 
     mov byte [gs:160], 'P'
+    mov byte [gs:161], 0x70
 
     jmp $
