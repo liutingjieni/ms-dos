7d2dc47ed8da27a377fd343917ffe567731584f3
liutingjieni <644123183@qq.com>
2020-11-06 22:15:55 +0800

    add

--- MASM/my_kernel/command.sh
+++ MASM/my_kernel/command.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-nasm -f elf -o lib/kernel/print.o lib/kernel/print.asm && gcc -I lib/kernel/ -c -o kernel/main.o kernel/main.c -m32 && ld -Ttext 0xc0001500 -e main  -m elf_i386 -o kernel/kernel.bin kernel/main.o lib/kernel/print.o && dd if=kernel/kernel.bin of=~/c.img bs=512 count=200 seek=9 conv=notrunc
+#!/bin/bash 
+gcc -I lib/kernel/ -I lib/ -I kernel -c -fno-builtin -o main.o kernel/main.c -m32
+nasm -f elf -o build/print.o lib/kernel/print.asm && 
+nasm -f elf -o build/kernel.o kernel/kernel.asm && 
+gcc -I lib/kernel/ -I lib -I kernel/ -c -fno-builtin -o build/interrupt.o build/interrupt.c -m32 && 
+gcc -I lib/kernel/ -I lib -I kernel/ -c -fno-builtin -o build/init.o kernel/init.c -m32 && 
+ld -Ttext 0xc0001500 -e main  -m elf_i386 -o build/kernel.bin build/main.o build/init.o build interrupt.o build/print.o build/kernel.o && 
+dd if=kernel/kernel.bin of=~/c.img bs=512 count=200 seek=9 conv=notrunc
 
