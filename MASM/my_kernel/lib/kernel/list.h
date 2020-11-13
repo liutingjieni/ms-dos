edf66861d957894610d9e0f1dba665a6a5e830d3
liutingjieni <644123183@qq.com>
2020-11-13 22:54:16 +0800

    多线程

---
+++ MASM/my_kernel/lib/kernel/list.h
@@ -0,0 +1,41 @@
+/*************************************************************************
+	> File Name: list.h
+	> Author:jieni 
+	> Mail: 
+	> Created Time: 2020年11月13日 星期五 21时46分39秒
+ ************************************************************************/
+
+#ifndef _LIST_H
+#define _LIST_H
+#include "global.h"
+
+#define offset(struct_type, member) (int)(&((struct_type*)0)->member)
+#define elem2entry(struct_type, struct_member_name, elem_ptr) \
+        (struct_type*)((int)elem_ptr - offset(struct_type, struct_member_name))
+
+/******************定义链表成员结构**************
+ * 节点中不需要数据成员, 只要求前驱和后继节点指针*/
+struct list_elem {
+    struct list_elem* prev;
+    struct list_elem* next;
+};
+
+struct list {
+    struct list_elem head;
+    struct list_elem tail;
+};
+
+typedef bool (function)(struct list_elem*, int arg);
+
+void list_init(struct list*);
+void list_insert_before(struct list_elem* before, struct list_elem* elem);
+void list_push(struct list* plist, struct list_elem* elem);
+void list_iterate(struct list* plist);
+void list_append(struct list *plist, struct list_elem *elem);
+void list_remove(struct list_elem *pelem);
+struct list_elem *list_pop(struct list *plist);
+bool list_empty(struct list *plist);
+uint32_t list_len(struct list *plist);
+struct list_elem *list_traversal(struct list *plist, function func, int arg);
+bool elem_find(struct list *plist, struct list_elem *obj_elem);
+#endif
