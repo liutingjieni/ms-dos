b386ff874831d7ffb279353e95dd1daecd60f426
liutingjieni <644123183@qq.com>
2020-11-23 21:38:37 +0800

    初始化tss

--- MASM/my_kernel/kernel/global.h
+++ MASM/my_kernel/kernel/global.h
@@ -45,6 +45,8 @@
 #define SELECTOR_U_DATA ((6 << 3) + (TI_GDT << 2) + RPL3)
 #define SELECTOR_U_STACK SELECTOR_U_DATA
 #define GDT_ATTR_HIGH ((DESC_G_4K << 7) + (DESC_D_32 << 6) + (DESC_L << 5) + (DESC_AVL << 4))
+#define GDT_CODE_ATTR_LOW_DPL3 ((DESC_P << 7) + (DESC_DPL_3 << 5) + (DESC_S_CODE << 4) + DESC_TYPE_CODE)
+#define GDT_DATA_ATTR_LOW_DPL3 ((DESC_P << 7) + (DESC_DPL_3 << 5) + (DESC_S_DATA << 4) + DESC_TYPE_DATA)
 /*----------------------GDT属性-------------------------*/
 #define IDT_DESC_P 1
 #define IDT_DESC_DPL0 0
@@ -56,7 +58,18 @@
 #define IDT_DESC_ATTR_DPL3 ((IDT_DESC_P << 7) + (IDT_DESC_DPL3 << 5) + IDT_DESC_32_TYPE)
 
 
+#define TSS_DESC_D 0
+#define TSS_ATTR_HIGH ((DESC_G_4K << 7) + (TSS_DESC_D << 6) + (DESC_L << 5) + (DESC_AVL << 4) + 0x0)
+#define TSS_ATTR_LOW ((DESC_P << 7) + (DESC_DPL_0 << 5) + (DESC_S_SYS << 4) + DESC_TYPE_TSS)
 
+#define SELECTOR_TSS ((4 << 3) + (TI_GDT << 2) + RPL0)
 
-#define 
+struct gdt_desc {
+   uint16_t limit_low_word;
+   uint16_t base_low_word;
+   uint8_t base_mid_byte;
+   uint8_t attr_low_byte;
+   uint8_t limit_high_attr_high;
+   uint8_t base_high_byte;
+};
 #endif
--- MASM/my_kernel/kernel/init.c
+++ MASM/my_kernel/kernel/init.c
@@ -12,6 +12,8 @@
 #include "thread.h"
 #include "console.h"
 #include "keyboard.h"
+#include "tss.h"
+
 void init_all()
 {
     put_str("init_all\n");
@@ -21,4 +23,5 @@ void init_all()
     timer_init();
     console_init();
     keyboard_init();
+    tss_init();
 }
